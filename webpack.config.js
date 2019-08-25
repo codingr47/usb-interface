b4c9e7143951d0cec80efa59d58074f5ec57dc04
Codingr <codingr@gmail.com>
2019-08-25 23:17:23 +0300

    added default route to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,10 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, '/'),
         compress: true,
-        port: 8080
+        port: 8080,
+        historyApiFallback: {
+            index: 'client/index.html'
+        }
     },
     output: {
         publicPath: '/',
